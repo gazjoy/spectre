5934054de9172be02a95dbff50bf46702704485b
Gareth.Joyce <gareth.joyce@GarethJoyces-MacBook-Pro.local>
2018-02-08 23:02:24 +0000

    Adding sass and colour setup.

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,13 @@
 import './modules'
 
 console.log(`app.js has loaded!`)
+
+
+const siteBody = document.querySelector('.ui-site-wrapper');
+
+document.querySelector('.js-nav-toggle').addEventListener('click', e => {
+  e.preventDefault();
+
+  siteBody.classList.toggle('is-active');
+  document.querySelector('.ui-nav').classList.toggle('is-active');
+});
