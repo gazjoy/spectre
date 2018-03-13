6b67dc598fa557afb1e389246ef473b5380a158b
Gareth.Joyce <gareth.joyce@GarethJoyces-MacBook-Pro.local>
2018-03-13 22:42:43 +0000

    Progress on some a11y bits in the ui, also adds active nav class.

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -2,11 +2,36 @@ import './modules'
 
 console.log(`app.js has loaded!`)
 
-const siteBody = document.querySelector('.ui-site-wrapper');
+const siteBody = document.querySelector('body');
 
-document.querySelector('.js-nav-toggle').addEventListener('click', e => {
-  e.preventDefault();
-
-  siteBody.classList.toggle('is-active');
-  document.querySelector('.ui-nav').classList.toggle('is-active');
+// Adds function to toggle a class for
+// mobile navigation element display
+function toggleNav (e) {
+    siteBody.classList.toggle('is-active');
+    document.querySelector('.ui-nav-mobile').classList.toggle('is-active');
+}
+document.querySelector('.ui-menu-btn').addEventListener('click', e => {
+    e.preventDefault();
+    toggleNav();
+    document.querySelector('.ui-menu-close').focus();
 });
+
+document.querySelector('.ui-menu-close').addEventListener('click', e => {
+    e.preventDefault();
+    toggleNav();
+    document.querySelector('.ui-menu-btn').focus();
+});
+
+
+// Adds an active class to the
+// current page nav item
+// Adapted from https://css-tricks.com/snippets/jquery/add-active-navigation-class-based-on-url/
+var nav     = document.getElementById('nav'),
+    anchor  = nav.getElementsByTagName('a'),
+    current = window.location.pathname;
+    
+for (var i = 0; i < anchor.length; i++) {
+    if(anchor[i].pathname == current) {
+        anchor[i].className = "is-active";
+    }
+}
